d053fbf0f3b6a184e003c440bddd799c5390fbba
RAZYANIK <54760996+RAZYANIK@users.noreply.github.com>
2020-09-30 02:03:27 +0600

    Add files via upload

---
+++ SystemMethods.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managemnetSystem;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collections;
+public class SystemMethods {
+    
+    
+    public boolean login(String username, String password){
+        String u = username;
+        String p = password;
+        if(u.equals("Admin") && p.equals("Admin")){
+            return true;
+        }
+        else{
+        return false;
+        }
+    }
+   
+    public ArrayList<String> testAddStudentName() {
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("Farhan");
+
+    return(arr);
+    }
+     public ArrayList<String> testAddStudentemail() {
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("xyz@gamil.com");
+
+    return(arr);
+    }
+      public ArrayList<String> testAddStudentRoll() {
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("18101480");
+
+    return(arr);
+    }
+      public ArrayList<String> testAddStudentAddress() {
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("Mirpur Dhaka");
+
+    return(arr);
+    } 
+    
+    
+    public ArrayList<String> RemoveEmployee(){
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.clear();
+    return arr;   
+    }
+    
+    
+    public String UpdateStudentList(){
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("Faisal");
+    
+    arr.add("2");
+    arr.add("Karim");
+    arr.add("3");
+    arr.set(4, "Aryan");
+    return arr.get(4);
+    }
+    
+    public boolean SearchStudent(){
+    ArrayList<String> arr = new ArrayList<String>();
+    if(!arr.contains("Aryan")){
+      return true;   
+    }
+    return false;
+    }
+    
+
+    
+    
+    public String ViewStudentDetails(){
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("Farhan");
+    arr.add("20");
+    arr.add("2");
+    arr.add("Aryan");
+    arr.add("22");
+    if(!arr.isEmpty()){
+       return "You can now view the details"; 
+    }
+    return "Please fillup all the columns";
+    }
+   
+       
+ 
+    public String StudentTotalFeeCounter(int s){
+          if(s <5000){
+             return "Approved";
+          }
+          else{
+             return "Declined";
+              }
+     }
+    public String ViewStudentAnnouncements(){
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("15 august school off");
+    arr.add("2");
+   
+    arr.add("ct start from 15 sept");
+    arr.add("22");
+    if(!arr.isEmpty()){
+       return "Final starts from 5th october 2020"; 
+    }
+    return "Please fillup all the columns";
+    }
+    
+    public int StudentReportCard(int a,int b,int c,int d,int e){
+        
+        int english = a;
+        int physics = b;
+        int chemistry = c;
+        int math = d;
+        int biology = e;
+        
+        
+
+        int totalmark=chemistry+physics+math+biology+english;
+        int percentage=(totalmark/5);
+        return percentage;
+        
+    
+    }
+    public String StudentService(){
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("1");
+    arr.add("Mr samiul islam");
+    arr.add("2");
+   
+    arr.add("Tanvir rahman");
+    arr.add("22");
+    if(!arr.isEmpty()){
+       return "mail your faculty for appoinments"; 
+    }
+    return "no faculty is available ";
+    }
+    
+  
+    
+    
+}
---
+++ SystemMethodsTest.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managemnetSystem;
+
+import managemnetSystem.SystemMethods;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.Collections.list;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class SystemMethodsTest {
+    
+    SystemMethods m;
+   
+    
+    @Before
+    public void setUp() throws Exception {
+        m = new SystemMethods();
+    }
+      
+    
+    @Test
+    public void testLogin(){
+        String u = "Admin";
+        String p = "Admin";
+        
+        boolean b = m.login(u, p);
+        assertTrue(b);
+        
+    }
+    
+    @Test
+    public void testAddStudentName() {
+      assertEquals(Arrays.asList("1", "Farhan"), m.testAddStudentName());
+    }
+
+    public void testAddStudentRoll() {
+      assertEquals(Arrays.asList("1", "18101480"), m.testAddStudentRoll());
+    }
+    public void testAddStudentAddress() {
+      assertEquals(Arrays.asList("1", "Mirpur Dhaka"), m.testAddStudentAddress());
+    }
+   
+    @Test
+    public void testRemoveStudent(){
+     assertEquals(Arrays.asList(), m.RemoveEmployee());
+    }
+    
+    @Test
+    public void testUpdateStudentList(){
+        
+      assertEquals("Aryan",m.UpdateStudentList());    
+    
+    }
+   
+    @Test
+    public void testSearchStudent(){
+     
+       assertEquals(Arrays.asList("1","farhan", "20","2","karim","22","3","Aryan","24").contains("Aryan"), m.SearchStudent());
+        
+    }
+    
+    @Test
+    public void testViewStudentDetails(){   
+        assertEquals("You can now view the details",m.ViewStudentDetails());   
+    }
+     
+    
+
+   
+    @Test
+    public void testStudentTotalFeeCounter() {
+        
+       assertEquals("Approved",m.StudentTotalFeeCounter(500));
+    }
+    @Test
+    public void testViewStudentAnnouncements(){   
+        assertEquals("Final starts from 5th october 2020",m.ViewStudentAnnouncements());   
+    }
+   
+    
+    @Test
+    public void testStudentReportCard(){
+        
+        assertEquals(83,m.StudentReportCard(95,85,75,86,78));  
+    
+   }
+    
+    @Test
+    public void testStudentService(){
+   
+     assertEquals("mail your faculty for appoinments",m.StudentService());
+      
+    }
+    
+    }
+    
+    
+   
+
+    
+    
+   
+    
+   
+    
+    
+    
+
